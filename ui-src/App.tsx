6c7d34ac6b733c2f0f268f3255f939048d2b428d
zomdar <zomdar@gmail.com>
2022-10-21 00:36:36 -0400

    adding new api endpoint

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+// url to hit
+// price: https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true
+// list of coin info: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc
+// list of coin symbols: https://api.coingecko.com/api/v3/simple/supported_vs_currencies
+// list of price changes to plot on graph: https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1
+
 function App() {
   useEffect(() => {
     if (typeof parent !== undefined) {
@@ -22,7 +28,8 @@ function App() {
           const name = document.getElementById("name") as HTMLInputElement;
           const value = name.value.toLowerCase();
 
-          fetch("https://api.coingecko.com/api/v3/coins/" + value)
+
+          fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${value}&vs_currencies=usd&include_24hr_change=true`)
             .then((res) => res.json())
             .then(
               (result) => {
@@ -39,7 +46,7 @@ function App() {
                         change: result.market_data.price_change_24h,
                         changePercent:
                           result.market_data.price_change_percentage_24h,
-                        lastUpdated: result.last_updated,
+                        lastUpdated: Date.now(),
                       },
                     },
                   },
